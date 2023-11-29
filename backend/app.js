ff8429938b0548aa011dc303284c874fa3d276dd
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-11-28 22:43:13 -0700

    callback url

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.callbackURL
+  callbackURL: process.env.CALLBACK_URL,
 },
 function (accessToken, refreshToken, profile, done) {
   // User.findOrCreate({ githubId: profile.id }, function (err, user) {
